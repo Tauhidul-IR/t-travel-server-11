150f0fdcbea579773442a4c5caae2b9b2a7df316
Tauhidul Islam Robin <robin13690@gmail.com>
2022-11-09 10:36:00 +0600

    delete review

--- index.js
+++ index.js
@@ -68,6 +68,14 @@ async function run() {
             res.send(reviews);
         })
 
+        //delete 
+        app.delete('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await reviewCollection.deleteOne(query);
+            res.send(result)
+        })
+
     }
     finally {
 
